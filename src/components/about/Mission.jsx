d042d34b88d1abe90950600b6b3b609128500bef
Nnaemekac <nnaemekachimezie5@gmail.com>
2024-08-23 15:30:51 -0700

    ---

--- src/components/about/Mission.jsx
+++ src/components/about/Mission.jsx
@@ -8,7 +8,7 @@ export default function Mission(){
             <section className="xui-pt-8 xui-pb-2">
                 <div className="xui-d-flex xui-flex-ai-center xui-lg-flex-dir-row xui-flex-dir-column-reverse xui-container">
                     <div className="xui-lg-col-5 xui-col-12">
-                        <SectionDuplicate paragraphHeading="Our Mission" mainHeading="Our Mission Statement" mainParagraph="At Good Life Spa, our mission is to enhance your well-being through personalized, high-quality spa treatments, creating a serene environment that promotes relaxation, rejuvenation, and holistic health.">
+                        <SectionDuplicate paragraphHeading="Our Mission" mainHeading="Our Mission Statement" mainParagraph="At the good life spa our mission is to create a serene and welcoming space where guests can escape the stresses of everyday life and experience a deeper sense of calm and wellness .we are dedicated to providing personalized results driven treatments and exceptional customer service guiding our guests on their journey towards relaxation ,rejuvenation and self discovery.">
                             {/* <LinkButton text="Learn More" route="/" customClass="xui-btn xui-bdr-rad-1-half xui-mt-1" /> */}
                         </SectionDuplicate>
                     </div>
--- src/components/about/Teams.jsx
+++ src/components/about/Teams.jsx
@@ -1,6 +1,13 @@
-import TeamImage from "../../assets/images/team.png";
+import TeamImage from "../../assets/images/team1.jpeg";
+import TeamImage2 from "../../assets/images/team2.jpeg";
 import LinkButton from "../LinkButton";
 import SectionDuplicate from "../SectionDuplicate";
+import { EffectFade, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export default function Teams(){
     return(
@@ -8,7 +15,23 @@ export default function Teams(){
             <section className="xui-pt-5 xui-pb-2">
                 <div className="xui-d-flex xui-flex-ai-center xui-lg-flex-dir-row xui-flex-dir-column-reverse xui-container">
                     <div className="xui-lg-col-6 xui-col-12">
-                        <img className="xui-img-600" src={TeamImage} alt="" />
+                        <Swiper
+                        className="team-swiper"
+                        slidesPerView={1}
+                        spaceBetween={20}
+                        effect='fade'
+                        modules={[Autoplay, EffectFade]}
+                        autoplay={true}
+                    >
+                        <SwiperSlide>
+                            <img className="xui-w-fluid-100 xui-h-fluid-100 xui-bdr-rad-1" src={TeamImage} alt="" />
+                        </SwiperSlide>
+                        {/* Repeat SwiperSlide for each slide */}
+                        <SwiperSlide>
+                            <img className="xui-w-fluid-100 xui-h-fluid-100 xui-bdr-rad-1" src={TeamImage2} alt="" />
+                        </SwiperSlide>
+                        
+                    </Swiper>
                     </div>
                     <div className="xui-lg-col-6 xui-col-12 xui-lg-pl-2 xui-pl-none xui-lg-mb-none xui-mb-1-half">
                         <SectionDuplicate paragraphHeading="The Team" mainHeading="Meet The Team" mainParagraph="Dedicated professionals committed to your wellness journey, who bring expertise, passion, and personalized care to every visit at Good Life Spa. Together, we strive to exceed your expectations and enhance your well-being through our holistic approach to spa services.">
